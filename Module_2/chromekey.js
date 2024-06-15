f1e96997251a5eae7aac6da9f182eca0b9dfcbf9
olvak <82042917+olvak@users.noreply.github.com>
2024-06-15 16:31:29 +0300

    add chromekey.js

---
+++ Module_2/chromekey.js
@@ -0,0 +1,25 @@
+// Set the foreground image (fgImage)
+let fgImage = new SimpleImage ("somefgimage.png");
+// Set the background image (bgImage)
+let bgImage = new SimpleImage("somebgimage.png");
+// Create a blank image with the same size (outpu)
+let output = new SimpleImage(fgImage.getWidth(), fgImage.getHeight());
+
+// For each pixel (currentPixel) e fgImage:
+for (let pixel of fgImage.values()) {
+    // Check the (currentPixel). If it's green:
+    if (pixel.getGreen() > pixel.getRed() + pixel.getBlue()){
+        // Find pixel at the same place of bgImage
+        let x = pixel.getX();
+        let y = pixel.getY();
+        let bgPixel = bgImage.getPixel(x, y);
+        // Set the output pixel according to bgImage pixel
+        output.setPixel(x, y, bgPixel);
+    }
+    // Else: set output pixel according to currentPixel
+    else {
+        output.setPixel(pixel.getX(), pixel.getY(), pixel);
+    }
+}
+// Show the result!
+print(output);
