e3800b127e50d4b9cce0c9d5fc017e6f23480ad2
olvak <82042917+olvak@users.noreply.github.com>
2024-06-17 16:04:37 +0300

    start Module 3

---
+++ Module_2/redfilter.js
@@ -0,0 +1,18 @@
+function filterRed(image) {
+    for (let pixel of image.values()) {
+        let avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+      if (avg < 128) {
+        pixel.setRed(2 * avg);
+        pixel.setGreen(0);
+        pixel.setBlue(0);
+      } else {
+        pixel.setRed(255);
+        pixel.setGreen(2 * avg - 255);
+        pixel.setBlue(2 * avg - 255);
+      }
+    }
+}
+  
+let img = new SimpleImage("imagename.png");
+filterRed(img);
+print (img);
---
+++ Module_3/script/change.js
@@ -0,0 +1,13 @@
+function changeColor() {
+    let divElement1 = document.getElementById("div1");
+    let divElement2 = document.getElementById("div2");
+    divElement1.className = "blueback";
+    divElement2.className = "darkorange";
+}
+
+function changeText() {
+    let divElement1 = document.getElementById("div1");
+    let divElement2 = document.getElementById("div2");
+    divElement1.className = "Erste";
+    divElement2.className = "Zweite";
+}
